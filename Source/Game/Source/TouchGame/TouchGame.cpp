1e777dfec1b18c3304ca4c7195a2957046f5703f
Коптяев А. В <a.koptyaev@playrix.com>
2022-11-05 21:08:00 +0600

    Вырезал старую игровую логику

--- Source/Game/Source/TouchGame/TouchGame.cpp
+++ Source/Game/Source/TouchGame/TouchGame.cpp
@@ -33,9 +33,7 @@ Object lightObject;
 vec3 lightPos(-500.0f, -500.0f, 500.0f);
 
 TouchGame::TouchGame()
-	: _indexCurrentMap(-1)
-	, _updateTime(0)
-	, _state(State::MENU)
+
 {
 }
 
@@ -61,64 +59,12 @@ void TouchGame::init() {
 
 	DRAW::setClearColor(0.3f, 0.6f, 0.9f, 1.0f);
 
-	if (_indexCurrentMap == -1) {
-		{
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Apartment_00", visibleMap));
-		}
-		{
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Apartment_01", visibleMap));
-		}
-		/* {
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Apartment_02", visibleMap));
-		}
-		{
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Apartment_03", visibleMap));
-		}*/
-
-		/* {
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Room_00", visibleMap));
-		}
-		{
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Room_01", visibleMap));
-		}
-		{
-			std::map<std::string, bool> visibleMap;
-			_maps.emplace_back(std::pair("Room_02", visibleMap));
-		}*/
-
-		_indexCurrentMap = 0;
-		_state = TouchGame::State::GAME;
-	}
-
-	//lightObject.set("light", "Lamp_00", lightPos);
-
 	update();
 	initCallback();
 }
 
 void TouchGame::update() {
-	if (_state == State::MENU) {
-		return;
-	}
-	else if (_state == State::EXIT) {
-		Engine::Core::close();
-		return;
-	}
 
-	/*double currentTime = Engine::Core::currentTime();
-	if (_updateTime > currentTime) {
-		return;
-	}
-
-	_updateTime = currentTime + 3000;
-
-	std::map<std::string, bool>& visibleMap = _maps[_indexCurrentMap].second;*/
 }
 
 void TouchGame::draw() {
@@ -127,99 +73,72 @@ void TouchGame::draw() {
 
 	// Draw
 	DRAW::prepare();
-
-	if (_state == TouchGame::State::MENU) {
-		DRAW::draw(*Map::getByName("Menu"));
-	}
-	else if (_state == TouchGame::State::EXIT) {
-		Engine::Core::close();
-	}
-	else {
-		DRAW::draw(*Map::getByName(_maps[_indexCurrentMap].first));
-	}
-
-	// Свет
-	Draw::prepare();
-	Draw::draw(lightObject);
+	DRAW::draw(*Map::getByName("Menu"));
 }
 
 void TouchGame::Drawline() {
 	DrawLine::prepare();
 
-	if (_qwe0_) {
-		{
-			float points[] = { 0.0f, 0.0f, 0.0f, 10.0f, 20.0f, 20.0f,
-								10.0f, 20.0f, 20.0f, 20.0f, 20.0f, 20.0f };
-			Line line(points, 4, Line::LINE);
-			line.setLineWidth(5.0f);
-			line.color = Color::GREEN;
+	{
+		float points[] = { 0.0f, 0.0f, 0.0f, 10.0f, 20.0f, 20.0f,
+							10.0f, 20.0f, 20.0f, 20.0f, 20.0f, 20.0f };
+		Line line(points, 4, Line::LINE);
+		line.setLineWidth(5.0f);
+		line.color = Color::GREEN;
 
-			DrawLine::draw(line);
-		}
+		DrawLine::draw(line);
+	}
 
-		{
-			float points[] = { 20.0f, 30.0f, 0.0f,
-								20.0f, 30.0f, 20.0f,
-								30.0f, 30.0f, 20.0f };
-			Line line(points, 3, Line::LOOP);
-			line.setLineWidth(5.0f);
-			line.color = { 0.3f, 0.6f, 0.9f,0.5f };
+	{
+		float points[] = { 20.0f, 30.0f, 0.0f,
+							20.0f, 30.0f, 20.0f,
+							30.0f, 30.0f, 20.0f };
+		Line line(points, 3, Line::LOOP);
+		line.setLineWidth(5.0f);
+		line.color = { 0.3f, 0.6f, 0.9f,0.5f };
 
-			DrawLine::draw(line);
-		}
+		DrawLine::draw(line);
+	}
 
-		{
-			float points[] = { 30.0f, 40.0f, 0.0f,
-								30.0f, 40.0f, 20.0f,
-								40.0f, 40.0f, 20.0f };
-			Line line(points, 3, Line::STRIP);
-			line.setLineWidth(5.0f);
-			line.color = Color::RED;
-			line.color.setAlpha(0.5);
+	{
+		float points[] = { 30.0f, 40.0f, 0.0f,
+							30.0f, 40.0f, 20.0f,
+							40.0f, 40.0f, 20.0f };
+		Line line(points, 3, Line::STRIP);
+		line.setLineWidth(5.0f);
+		line.color = Color::RED;
+		line.color.setAlpha(0.5);
 
-			DrawLine::draw(line);
-		}
-
-		/* {
-			float pointsX[] = { 0.f, 0.f, 0.f, _lenghtNormal, _lenghtNormal, _lenghtNormal };
-			Line lineX(pointsX, 2, Line::LINE);
-			lineX.setLineWidth(5.0f);
-			lineX.color = Color::RED;
-			lineX.color.setAlpha(0.999);
-			DrawLine::draw(lineX);
-		}*/
+		DrawLine::draw(line);
 	}
 
 	DrawLine::draw(*_greed);
-
-	//...
-	if (_qwe_) {
-		Map& map = *Map::getByName(_maps[_indexCurrentMap].first);
-
-		for (auto& object : map.objects) {
-			if (object->visible()) {
-				float* vertexes = object->getModel().getMesh().vertexes();
-				float* normals = object->getModel().getMesh().normals();
-				int countVertex = object->getModel().getMesh().countVertex();
-
-				for (int i = 0; i < countVertex * 3; i = i + 3) {
-					float* pos = &vertexes[i];
-					float* norm = &normals[i];
-
-					float pointsX[] = { pos[0], pos[1], pos[2], pos[0] + norm[0] * _lenghtNormal, pos[1] + norm[1] * _lenghtNormal, pos[2] + norm[2] * _lenghtNormal };
-					Line lineX(pointsX, 2, Line::LINE);
-					lineX.setLineWidth(_widthNormal);
-					lineX.color = Color::RED;
-					lineX.color.setAlpha(0.75);
-					DrawLine::draw(lineX, object->getMatrix());
-				}
-			}
-		}
-	}
 }
 
 void TouchGame::resize() {
 	Camera::getCurrent().setPerspective(Camera::getCurrent().fov(), Engine::Screen::aspect(), 0.1f, 1000.0f);
+}
+
+void TouchGame::CheckMouse() {
+	glm::vec2 mousePos = Engine::Callback::mousePos();
+
+	if (mousePos.x < 0.f) {
+		mousePos.x = Engine::Screen::width() - mousePos.x;
+		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
+	}
+	else if (mousePos.x > Engine::Screen::width()) {
+		mousePos.x = mousePos.x - Engine::Screen::width();
+		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
+	}
+
+	if (mousePos.y < 0.f) {
+		mousePos.y = Engine::Screen::height() - mousePos.y;
+		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
+	}
+	else if (mousePos.y > Engine::Screen::height()) {
+		mousePos.y = mousePos.y - Engine::Screen::height();
+		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
+	}
 }
 
 void TouchGame::initCallback() {
@@ -233,12 +152,10 @@ void TouchGame::initCallback() {
 
 		if (Engine::Callback::pressTap(Engine::VirtualTap::RIGHT)) {
 			Camera::current.rotate(Engine::Callback::deltaMousePos());
-			CheckMouse();
 		}
 
 		if (Engine::Callback::pressTap(Engine::VirtualTap::MIDDLE)) {
 			Camera::current.move(Engine::Callback::deltaMousePos() * Engine::Core::deltaTime());
-			CheckMouse();
 		}
 
 		if (Engine::Callback::pressTap(Engine::VirtualTap::LEFT)) {
@@ -280,52 +197,9 @@ void TouchGame::initCallback() {
 		Engine::VirtualKey key = releaseKeyEvent->getId();
 
 		if (key == Engine::VirtualKey::ESCAPE) {
-			_state = State::MENU;
-			Map& map = currentMap();
-			map.load();
-			Camera::setCurrent(map.getCamera());
+			Engine::Core::close();
 		}
-
-		if (key == Engine::VirtualKey::S && Engine::Callback::pressKey(Engine::VirtualKey::CONTROL)) {
-			save();
-		}
-
-		if (key == Engine::VirtualKey::L && Engine::Callback::pressKey(Engine::VirtualKey::CONTROL)) {
-			load();
-		}
-
-		if (key == Engine::VirtualKey::Q) {
-			changeMap(false);
-			Map& map = currentMap();
-			Camera::setCurrent(map.getCamera());
-		}
-
-		if (key == Engine::VirtualKey::E) {
-			changeMap(true);
-			Map& map = currentMap();
-			Camera::setCurrent(map.getCamera());
-		}
-
-		if (key == Engine::VirtualKey::R && Engine::Callback::pressKey(Engine::VirtualKey::CONTROL) && Engine::Callback::pressKey(Engine::VirtualKey::SHIFT)) {
-			Model::clear();
-			Model::removeData();
-			Texture::clear();
-			Shape::clear();
-
-			Map& map = currentMap();
-			map.load();
-			Camera::setCurrent(map.getCamera());
-		}
-		else if (key == Engine::VirtualKey::R && Engine::Callback::pressKey(Engine::VirtualKey::CONTROL)) {
-			Map& map = currentMap();
-			map.load();
-			Camera::setCurrent(map.getCamera());
-		}
-
-		if (key == Engine::VirtualKey::T) {
-			DrawLight::resetShader();
-		}
-		});
+	});
 
 	_callbackPtr->add(Engine::CallbackType::PINCH_KEY, [this](const Engine::CallbackEventPtr& callbackEventPtr) {
 		if (Editor::Console::IsLock()) {
@@ -360,72 +234,13 @@ void TouchGame::initCallback() {
 		if (Engine::Callback::pressKey(Engine::VirtualKey::F)) {
 			Camera::current.move(CAMERA_DOWN, kForce);
 		}
-
-	gml:vec3 posCam = Camera::current.pos();
-		if (posCam.z < 50.f) {
-			posCam.z = 50.f;
-			Camera::current.setPos(posCam);
-		}
-		if (posCam.z > 270.f) {
-			posCam.z = 270.f;
-			Camera::current.setPos(posCam);
-		}
-		if (posCam.x < -320.f) {
-			posCam.x = -320.f;
-			Camera::current.setPos(posCam);
-		}
-		if (posCam.x > 320.f) {
-			posCam.x = 320.f;
-			Camera::current.setPos(posCam);
-		}
-		if (posCam.y < -235.f) {
-			posCam.y = -235.f;
-			Camera::current.setPos(posCam);
-		}
-		if (posCam.y > 375.f) {
-			posCam.y = 375.f;
-			Camera::current.setPos(posCam);
-		}
-
-		// Свет
-		if (Engine::Callback::pressKey(Engine::VirtualKey::O)) {
-			lightPos.z += 1.0f;
-		}
-		if (Engine::Callback::pressKey(Engine::VirtualKey::L)) {
-			lightPos.z -= 1.0f;
-		}
-		if (Engine::Callback::pressKey(Engine::VirtualKey::H)) {
-			lightPos.x += 1.0f;
-		}
-		if (Engine::Callback::pressKey(Engine::VirtualKey::K)) {
-			lightPos.x -= 1.0f;
-		}
-		if (Engine::Callback::pressKey(Engine::VirtualKey::U)) {
-			lightPos.y += 1.0f;
-		}
-		if (Engine::Callback::pressKey(Engine::VirtualKey::J)) {
-			lightPos.y -= 1.0f;
-		}
-
-		lightObject.setPos(lightPos);
-		DRAW::setLightPos(lightPos.x, lightPos.y, lightPos.z);
-		});
+	});
 
 	_callbackPtr->add(Engine::CallbackType::MOVE, [this](const Engine::CallbackEventPtr& callbackEventPtr) {
 		if (Editor::Console::IsLock()) {
 			return;
 		}
-
-		if (_state == State::MENU) {
-			float currentMousePos[] = { Engine::Callback::mousePos().x, Engine::Screen::height() - Engine::Callback::mousePos().y };
-			if (_mousePos[0] != currentMousePos[0] && _mousePos[1] != currentMousePos[1]) {
-				_mousePos[0] = currentMousePos[0];
-				_mousePos[1] = currentMousePos[1];
-				hit(_mousePos[0], _mousePos[1]);
-			}
-		}
-		});
-
+	});
 
 	_callbackPtr->add(Engine::CallbackType::RELEASE_KEY, [this](const Engine::CallbackEventPtr& callbackEventPtr) {
 		Engine::KeyCallbackEvent* releaseKeyEvent = (Engine::KeyCallbackEvent*)callbackEventPtr->get();
@@ -436,7 +251,7 @@ void TouchGame::initCallback() {
 				UI::CloseWindow("Console");
 			}
 			else {
-				_editMapWindow = UI::ShowWindow<Editor::Console>();
+				UI::ShowWindow<Editor::Console>();
 			}
 		}
 
@@ -445,7 +260,7 @@ void TouchGame::initCallback() {
 				UI::CloseWindow("Edit model");
 			}
 			else {
-				_editMapWindow = UI::ShowWindow<Editor::ModelEditor>();
+				UI::ShowWindow<Editor::ModelEditor>();
 			}
 		}
 		if (key == Engine::VirtualKey::F2) {
@@ -453,33 +268,12 @@ void TouchGame::initCallback() {
 				UI::CloseWindow("Edit map");
 			}
 			else {
-				_editMapWindow = UI::ShowWindow<Editor::Map>();
+				UI::ShowWindow<Editor::Map>();
 			}
 		}
-		});
+	});
 }
 
-void TouchGame::CheckMouse() {
-	glm::vec2 mousePos = Engine::Callback::mousePos();
-
-	if (mousePos.x < 0.f) {
-		mousePos.x = Engine::Screen::width() - mousePos.x;
-		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
-	}
-	else if (mousePos.x > Engine::Screen::width()) {
-		mousePos.x = mousePos.x - Engine::Screen::width();
-		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
-	}
-
-	if (mousePos.y < 0.f) {
-		mousePos.y = Engine::Screen::height() - mousePos.y;
-		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
-	}
-	else if (mousePos.y > Engine::Screen::height()) {
-		mousePos.y = mousePos.y - Engine::Screen::height();
-		Engine::Core::SetCursorPos(mousePos.x, mousePos.y);
-	}
-}
 void TouchGame::initPhysic() {
 
 }
@@ -520,71 +314,13 @@ void TouchGame::save()
 #endif // _DEBUG
 }
 
-Map& TouchGame::currentMap() {
-	std::string currentmapStr;
-	if (_state == TouchGame::State::MENU) {
-		currentmapStr = "Menu";
-	}
-	else {
-		currentmapStr = _maps[_indexCurrentMap].first;
-	}
-
-	if (currentmapStr.empty()) {
-		currentmapStr = "Menu";
-	}
-
-	Map::Ptr mapPtr = Map::getByName(currentmapStr);
-	return *mapPtr;
-}
-
-void TouchGame::changeMap(const bool right) {
-	size_t size = _maps.size();
-	if (size < 2) {
-		return;
-	}
-
-	_indexCurrentMap += right ? 1 : -1;
-
-	if (_indexCurrentMap < 0) {
-		_indexCurrentMap = size - 1;
-	}
-	else if (_indexCurrentMap >= size) {
-		_indexCurrentMap = 0;
-	}
-}
-
 void TouchGame::hit(const int x, const int y, const bool action) {
-	std::set<std::string> objectsUnderMouse;
+	/*std::set<std::string> objectsUnderMouse;
 
 	for (Object* object : currentMap().objects) {
 		if (object->visible() && object->hit(x, y)) {
 			objectsUnderMouse.emplace(object->getName());
 		}
-	}
+	}*/
 
-	if (objectsUnderMouse.find("Door_00") != objectsUnderMouse.end() || objectsUnderMouse.find("Door_rotate_00") != objectsUnderMouse.end()) {
-		currentMap().getObjectByName("Door_00").setVisible(false);
-		currentMap().getObjectByName("Door_rotate_00").setVisible(true);
-
-		if (objectsUnderMouse.find("Door_rotate_00") != objectsUnderMouse.end() && action) {
-			_state = State::EXIT;
-		}
-	}
-	else if (objectsUnderMouse.find("Monitor_display_00") != objectsUnderMouse.end() || objectsUnderMouse.find("Monitor_display_01") != objectsUnderMouse.end()) {
-		currentMap().getObjectByName("Monitor_display_00").setVisible(false);
-		currentMap().getObjectByName("Monitor_display_01").setVisible(true);
-
-		if (objectsUnderMouse.find("Monitor_display_01") != objectsUnderMouse.end() && action) {
-			_state = State::GAME;
-			Map& map = currentMap();
-			map.load();
-			Camera::setCurrent(map.getCamera());
-		}
-	}
-	else {
-		currentMap().getObjectByName("Door_00").setVisible(true);
-		currentMap().getObjectByName("Door_rotate_00").setVisible(false);
-		currentMap().getObjectByName("Monitor_display_00").setVisible(true);
-		currentMap().getObjectByName("Monitor_display_01").setVisible(false);
-	}
 }
--- Source/Game/Source/TouchGame/TouchGame.h
+++ Source/Game/Source/TouchGame/TouchGame.h
@@ -13,12 +13,6 @@ typedef std::shared_ptr<Engine::Callback> CallbackPtr;
 
 class TouchGame final : public Engine::Game
 {
-	enum class State {
-		MENU,
-		GAME,
-		EXIT
-	};
-
 public:
 	TouchGame();
 	~TouchGame();
@@ -29,14 +23,12 @@ public:
 	void draw() override;
 	void resize() override;
 
-	void initCallback();
 	void CheckMouse();
+	void initCallback();
 	void initPhysic();
 	bool load();
 	void save();
 
-	Map& currentMap();
-	void changeMap(const bool right);
 	void hit(const int x, const int y, const bool action = false);
 	void Drawline();
 
@@ -46,18 +38,7 @@ public:
 private:
 	CallbackPtr _callbackPtr;
 	std::vector<std::pair<std::string, std::map<std::string, bool>>> _maps;
-	int _indexCurrentMap;
-	double _updateTime;
-	State _state;
-	float _mousePos[2];
-	float _cameraSpeed = 1.f;
 
 	Greed* _greed = nullptr;
-	float _lenghtNormal = 10.f;
-	float _widthNormal = 1.f;
-	bool _qwe0_ = true;
-	bool _qwe_ = true;
-	bool _type_ = false;
 
-	std::string _editMapWindow;
 };
